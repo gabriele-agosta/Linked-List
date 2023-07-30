bdc4ab157522aa41411d392abc5ca31688227daa
g.agosta02 <g.agosta02@gmail.com>
2023-07-30 19:16:08 +0200

    Added all necessary functionalities

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -3,19 +3,116 @@ require_relative 'node'
 class LinkedList
 
     def initialize()
-        @head = Node.new
-        @tail = Node.new
+        @head = nil
+        @tail = nil
     end
 
     def append(value)
         node = Node.new(value)
 
-        if @head.value.nil?
+        if @head.nil?
             @head = node
             @tail = @head
         else
             @tail.next_node = node
             @tail = @tail.next_node
         end
+    end
+
+    def prepend(value)
+        node = Node.new(value)
+
+        if @head.nil?
+            @head = node
+            @tail = @head
+        else
+            node.next_node = @head
+            @head = node
+        end
+    end
+
+    def size()
+        size = 0
+        temp = @head
+
+        until temp.nil? do 
+            size += 1
+            temp = temp.next_node
+        end
+
+        return size
+    end
+
+    def head()
+        return "( #{@head.value} )"
+    end
+
+    def tail()
+        return "( #{@tail.value} )"
+    end
+
+    def to_s()
+        values = traverse_list()
+        values << "nil"
+
+        values.each do |value| 
+            value.is_a?(Numeric) ? print("( #{value} ) -> ") : puts("#{value}")
+        end
+    end
+
+    def at(index)
+        values = traverse_list()
+        return values[index]
+    end
+
+    def pop()
+        temp = @head
+
+        until temp.next_node == @tail do
+            temp = temp.next_node
+        end
+
+        temp.next_node = nil
+        @tail = temp
+    end
+
+    def contains?(value)
+        contained = false
+        temp = @head
+
+        until temp.nil? do
+            if value == temp.value 
+                contained = true
+                return contained
+            end
+            temp = temp.next_node
+        end
+
+        return contained
+    end
+
+    def find(value)
+        index = 0
+        temp = @head
+
+        until temp.nil? do
+            return index if value == temp.value 
+            index += 1
+            temp = temp.next_node
+        end
+
+        return index
+    end
+
+    private
+    def traverse_list()
+        values = Array.new
+        temp = @head
+
+        until temp.nil? do
+            values << temp.value
+            temp = temp.next_node
+        end
+        return values
     end
 end
--- lib/main.rb
+++ lib/main.rb
@@ -5,3 +5,27 @@ linked_list = LinkedList.new
 linked_list.append(10)
 linked_list.append(15)
 linked_list.append(20)
+linked_list.append(25)
+
+linked_list.prepend(5)
+
+puts "Size of the list: #{linked_list.size()}"
+
+puts "Head of the list: #{linked_list.head()}"
+
+puts "Tail of the list: #{linked_list.tail()}"
+
+puts "Value at index 2: #{linked_list.at(2)}"
+
+linked_list.to_s()
+
+linked_list.pop()
+
+linked_list.to_s()
+
+puts "Size of the list: #{linked_list.size()}"
+
+puts "Is 15 contained?: #{linked_list.contains?(15)}"
+puts "Is 1 contained?: #{linked_list.contains?(1)}"
+
+puts "Index of 15: #{linked_list.find(15)}"
